c63c5db231f5a4762d31617d03e9bbcec3e21694
Anna Spysz <anna.spysz@stackery.io>
2019-07-11 10:58:55 -0700

    Updates

--- src/newItem/index.js
+++ src/newItem/index.js
@@ -1,12 +1,15 @@
 const AWS = require('aws-sdk');
+const uuid = require('node-uuid');
 
-exports.handler = async () => {
+exports.handler = async event => {
   const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const newEntry = JSON.parse(event.body);
   const params = {
     TableName: process.env.TABLE_NAME, // get the table name from the automatically populated environment variables
     Item: {
-      id: '1', // modify with each invoke so the id does not repeat
-      content: 'This is my content' // modify content here
+      id: uuid.v1(),
+      content: newEntry.content,
+      timestamp: newEntry.timestamp
     },
     ConditionExpression: 'attribute_not_exists(id)', // do not overwrite existing entries
     ReturnConsumedCapacity: 'TOTAL'
@@ -20,15 +23,16 @@ exports.handler = async () => {
     console.log(`Error writing to table ${process.env.TABLE_NAME}. Make sure this function is running in the same environment as the table.`);
     throw new Error(error); // stop execution if dynamodb is not available
   }
-  
+
   // Return a 200 response if no errors
   const response = {
-    statusCode: 200,
+    statusCode: "200",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
     },
-    body: 'Success!'
-  };
-
+    body: "Success",
+    isBase64Encoded: false
+  }
   return response;
 };
